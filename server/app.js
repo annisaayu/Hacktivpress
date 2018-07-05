efea9392596f18a699fc307d9ddb5853cd1fc6d6
annisaayu <annisaayu569@gmail.com>
2018-07-05 14:40:12 +0700

    added npm

--- server/app.js
+++ server/app.js
@@ -1,18 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var articlesRouter = require('./routes/articles');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+// const articlesRouter = require('./routes/articles');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+let dbuser = process.env.DBUSER
+let dbpassword = process.env.DBPASSWORD
+let url = `mongodb://${dbuser}:${dbpassword}@ds125381.mlab.com:25381/live-code`
+mongoose.connect(url)
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+    console.log('===== connected to mongoose =====')
+});
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/articles', articlesRouter);
+// app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
